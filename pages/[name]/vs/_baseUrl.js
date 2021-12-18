07857e55c00412bd049d044d6f1d4453edfa911f
Javier Carrasco <javier.carrasco@creditas.com>
2021-12-18 15:48:20 +0100

    attempt to fix random fight 21

--- pages/[name]/vs/_baseUrl.js
+++ pages/[name]/vs/_baseUrl.js
@@ -2,3 +2,7 @@ const dev = process.env.NODE_ENV !== "production";
 export const server = dev
   ? "http://localhost:3000"
   : "https://pokenext-navy.vercel.app/";
+
+export default function () {
+  return null;
+}
